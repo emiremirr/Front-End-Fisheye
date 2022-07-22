38871b456021a007988512302ef2d1185337063b
emiremirr <h_emir@outlook.fr>
2022-07-22 05:47:20 +0200

    ajout des éléments city, tagline, price via le dom

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,6 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+function photographerFactory(photographers) {
+    
+    const { name, portrait, city, country, tagline, price } = photographers;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -8,10 +9,26 @@ function photographerFactory(data) {
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const pPrice = document.createElement('p');
+        const pCityCountry = document.createElement('p');
+        const pTagline = document.createElement('p');
         h2.textContent = name;
+        pCityCountry.textContent = city+", "+country;
+        pCityCountry.setAttribute("class", "photographer_section_city_country")
+        pTagline.textContent = tagline;
+        pTagline.setAttribute("class", "photographer_section_city_tagline")
+        pPrice.textContent = price+"€/jour";
+        pPrice.setAttribute("class", "photographer_section_city_price")
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(pCityCountry)
+        article.appendChild(pTagline)
+        article.appendChild(pPrice)
         return (article);
     }
     return { name, picture, getUserCardDOM }
+
+    
+
+
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,33 +1,47 @@
-    async function getPhotographers() {
+async function getPhotographers() {
+    let photographers ;
+    await fetch  ('./data/photographers.json')
+    
+    .then(function (response) { return response.json()})
+    .then(function (data) {
+console.log(data.media)
+        photographers = data.photographers
+        console.log(photographers)}
+    
+    )
+    // Work with JSON data here
+      
+  .catch(err => {
+    // Do something for an error here
+  });
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        // const photographers = [
+        //     {
+        //         "name": "Ma data test",
+        //         "id": 1,
+        //         "city": "Paris",
+        //         "country": "France",
+        //         "tagline": "Ceci est ma data test",
+        //         "price": 400,
+        //         "portrait": "account.png"
+        //     },
+        //     {
+        //         "name": "Autre data test",
+        //         "id": 2,
+        //         "city": "Londres",
+        //         "country": "UK",
+        //         "tagline": "Ceci est ma data test 2",
+        //         "price": 500,
+        //         "portrait": "account.png"
+        //     },
+        // ]
         // et bien retourner le tableau photographers seulement une fois
+        
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: [...photographers]})
     }
-
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
-
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
