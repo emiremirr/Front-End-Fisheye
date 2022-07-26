c6a4803fac2548d018ebb418e606e9bf68b7c2be
emiremirr <h_emir@outlook.fr>
2022-07-26 06:51:14 +0200

    ajout du lien

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,26 +1,39 @@
 function photographerFactory(photographers) {
-    
-    const { name, portrait, city, country, tagline, price } = photographers;
+    console.log(photographers)    
+    const { name, portrait, city, country, tagline, price, id} = photographers;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
+        const link = document.createElement('a');
+        link.setAttribute("href","./photographer.html?id="+`${id}`)
+        link.appendChild(img)
         img.setAttribute("src", picture)
         img.setAttribute("alt", "photo de "+name)
+
         const h2 = document.createElement( 'h2' );
-        const pPrice = document.createElement('p');
-        const pCityCountry = document.createElement('p');
-        const pTagline = document.createElement('p');
         h2.textContent = name;
+
+        // création via le dom du paragraphe ville et pays
+        const pCityCountry = document.createElement('p');
         pCityCountry.textContent = city+", "+country;
         pCityCountry.setAttribute("class", "photographer_section_city_country")
+
+        // création via le dom du paragraphe slogan
+        const pTagline = document.createElement('p');
         pTagline.textContent = tagline;
         pTagline.setAttribute("class", "photographer_section_city_tagline")
+
+        // création via le dom du paragraphe prix
+        const pPrice = document.createElement('p');
         pPrice.textContent = price+"€/jour";
         pPrice.setAttribute("class", "photographer_section_city_price")
-        article.appendChild(img);
+
+
+        // insertion des éléments
+        article.appendChild(link);
         article.appendChild(h2);
         article.appendChild(pCityCountry)
         article.appendChild(pTagline)
@@ -28,8 +41,4 @@ function photographerFactory(photographers) {
         return (article);
     }
     return { name, picture, getUserCardDOM }
-
-    
-
-
 }
